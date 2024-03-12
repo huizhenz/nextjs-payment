239a7a0a8c121496141f53b88cc733e332a26d64
Jang Hyejin <hyeee513@naver.com>
2024-03-13 02:18:41 +0900

    feat : 연동 toss payment SDK

--- src/components/payment/Payment.tsx
+++ src/components/payment/Payment.tsx
@@ -1,77 +1,12 @@
-"use client";
-
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Card } from "../ui/card";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { Input } from "@/components/ui/input";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+import CheckoutPage from "@/paymentWidget/Checkout";
 
 export default function Payment() {
-  const form = useForm();
-
   return (
-    <Card className={cn("w-[380px]")}>
-      <CardHeader>
-        <CardTitle>결제 수단</CardTitle>
-      </CardHeader>
-
-      <CardContent className="grid gap-4">
-        <Form {...form}>
-          <FormField
-            control={form.control}
-            name="phone"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel></FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="phone"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel></FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </Form>
-      </CardContent>
-
-      <CardFooter></CardFooter>
+    <Card className={cn("w-[32rem]")}>
+      <CheckoutPage />
     </Card>
   );
 }
